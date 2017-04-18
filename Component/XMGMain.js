8435b1da6db6d948c64838630281bd95031b3cbb
laibao101 <369632567@qq.com>
2017-04-18 14:50:10 +0800

    add component

--- Component/XMGMain.js
+++ Component/XMGMain.js
@@ -23,27 +23,28 @@ export default class Main extends Component{
 					icon={require('../ios/newsDemo/Images.xcassets/TabBar/tabbar_home.imageset/tabbar_home.png')}
 					title='首页'
 				>
-
+                    <Home></Home>
 				</TabBarIOS.Item>
 				{/* 发现 */}
 				<TabBarIOS.Item
 					icon={require('../ios/newsDemo/Images.xcassets/TabBar/tabbar_discover.imageset/tabbar_discover.png')}
 					title='发现'
 				>
+                    <Find></Find>
 				</TabBarIOS.Item>
 				{/* 消息 */}
 				<TabBarIOS.Item
 					icon={require('../ios/newsDemo/Images.xcassets/TabBar/tabbar_message_center.imageset/tabbar_message_center.png')}
 					title='消息'
 				>
-
+                    <Message></Message>
 				</TabBarIOS.Item>
 				{/* 我的 */}
 				<TabBarIOS.Item
 					icon={require('../ios/newsDemo/Images.xcassets/TabBar/tabbar_profile.imageset/tabbar_profile.png')}
 					title='我的'
 				>
-
+                    <Mine></Mine>
 				</TabBarIOS.Item>
 			</TabBarIOS>
 		);
